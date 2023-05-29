7d9faf7930aa95c317b58b12b7ec87e89cead1f7
Vojtěch Charouz <vojtacharouz@gmail.com>
2023-05-29 12:08:36 +0200

    First commit

---
+++ Main.js
@@ -0,0 +1,60 @@
+
+// let BNav = document.getElementById("BNav");
+
+// let SNAv = document.getElementById("SideNav");
+
+// let SB = SNAv.children;
+
+// let BCheck = false;
+// let SCheck = false;
+
+
+
+// function Show() {
+//     SNAv.style.display = "Flex";
+//     BCheck = true;
+//     SNAv.style.width = SNAv.style.width;
+
+// }
+
+// function Bleft() {
+//     BCheck = false;
+//     Hide();
+// }
+
+
+// function SEnter() {
+//     SCheck = true;
+//     console.log("x");
+
+// }
+// function Sleft() {
+//     SCheck = false;
+//     console.log("Y");
+//     Hide();
+// }
+
+
+// function Hide() {
+//     if (BCheck == true) {
+//         return;
+//     }
+
+//     if (SCheck == true) {
+
+//         return;
+//     }
+
+//     SNAv.style.display = "None";
+// }
+
+// BNav.addEventListener("mouseenter", Show);
+// BNav.addEventListener("mouseleave", Bleft);
+
+// SNAv.addEventListener("mouseenter", SEnter);
+// SNAv.addEventListener("mouseleave", Sleft);
+
+// for (let i = 0; i < SB.length; i++) {
+//     SB[i].addEventListener("mouseenter", SEnter);
+//     SB[i].addEventListener("mouseleave", Sleft);
+// }
